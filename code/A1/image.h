585fc0239242518ba2542a55b11b93ecb9d1b104
Talha-AppDev <talha.app.dev42@gmail.com>
2025-10-06 05:09:04 +0500

    Grayscale fun implemented

---
+++ code/A1/image.h
@@ -0,0 +1,353 @@
+#pragma once
+#include "pixel.h"
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+class Image
+{
+public:
+    int width;
+    int height;
+    int maxColorValue;
+    Pixel **pixels;
+    std::string format;
+
+    Image() : width(0), height(0), maxColorValue(255), pixels(nullptr), format("\0") {}
+
+    bool readFile(const string &filename)
+    {
+        ifstream file(filename, ios::binary);
+        if (!file.is_open())
+        {
+            cerr << "Error: Cannot open file " << filename << endl;
+            return false;
+        }
+
+        format = nextToken(file);
+        width = stoi(nextToken(file));
+        height = stoi(nextToken(file));
+        maxColorValue = stoi(nextToken(file));
+
+        char ch;
+        file.get(ch);
+
+        pixels = new Pixel *[height];
+        for (int i = 0; i < height; i++)
+            pixels[i] = new Pixel[width];
+
+        if (format == "P6")
+        {
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    unsigned char rgb[3];
+                    file.read((char *)rgb, 3);
+                    pixels[i][j].R = rgb[0];
+                    pixels[i][j].G = rgb[1];
+                    pixels[i][j].B = rgb[2];
+                }
+            }
+        }
+        else if (format == "P5")
+        {
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    unsigned char gray;
+                    file.read((char *)&gray, 1);
+                    pixels[i][j].R = gray;
+                    pixels[i][j].G = gray;
+                    pixels[i][j].B = gray;
+                }
+            }
+        }
+        else
+        {
+            cerr << "Error: Unsupported format " << format << endl;
+            return false;
+        }
+
+        file.close();
+        return true;
+    }
+
+    string nextToken(ifstream &file)
+    {
+        string token;
+        while (true)
+        {
+            file >> token;
+            if (token[0] == '#')
+            {
+                string comment;
+                getline(file, comment);
+                continue;
+            }
+            return token;
+        }
+    }
+
+    bool makeGrayscale(const string &filename) {
+
+        ofstream file(filename, ios::binary);
+        if (!file.is_open())
+        {
+            cerr << "Error: Cannot open file " << filename << endl;
+            return false;
+        }
+
+        format = "P5";
+        file << format << '\n';
+        file << width << " " << height << '\n';
+        file << maxColorValue << '\n';
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                unsigned char rgb[3] = {pixels[i][j].R, pixels[i][j].G, pixels[i][j].B};
+                unsigned char gray = (rgb[0] + rgb[1] + rgb[2]) / 3;    
+
+                file.write((const char *)&gray, 1);
+            }
+        }
+
+        file.close();
+        return true;
+    }
+
+    void showInfo()
+    {
+        cout << "Format: " << format << endl;
+        cout << "Width: " << width << endl;
+        cout << "Height: " << height << endl;
+        cout << "Max Color Value: " << maxColorValue << endl;
+    }
+
+    bool writeFile(const string &filename)
+    {
+        ofstream file(filename, ios::binary);
+        if (!file.is_open())
+        {
+            cerr << "Error: Cannot open file " << filename << endl;
+            return false;
+        }
+
+        file << format << '\n';
+        file << width << " " << height << "\n";
+        file << maxColorValue << '\n';
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                unsigned char rgb[3] = {
+                    pixels[i][j].R,
+                    pixels[i][j].G,
+                    pixels[i][j].B
+                };
+                file.write((const char *)rgb, 3);
+            }
+        }
+
+        file.close();
+        return true;
+    }
+
+    void makeBlack()
+    {
+        if (pixels == nullptr)
+        {
+            cerr << "Error: Image data is not loaded." << endl;
+            return;
+        }
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                pixels[i][j].R = 0;
+                pixels[i][j].G = 0;
+                pixels[i][j].B = 0;
+            }
+        }
+    }
+
+    void makeWhite()
+    {
+        if (pixels == nullptr)
+        {
+            cerr << "Error: Image data is not loaded." << endl;
+            return;
+        }
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                pixels[i][j].R = 255;
+                pixels[i][j].G = 255;
+                pixels[i][j].B = 255;
+            }
+        }
+    }
+
+    void makeRed()
+    {
+        if (pixels == nullptr)
+        {
+            cerr << "Error: Image data is not loaded." << endl;
+            return;
+        }
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                pixels[i][j].R = 255;
+                pixels[i][j].G = 0;
+                pixels[i][j].B = 0;
+            }
+        }
+    }
+
+    void makeYellow()
+    {
+        if (pixels == nullptr)
+        {
+            cerr << "Error: Image data is not loaded." << endl;
+            return;
+        }
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                pixels[i][j].R = 255;
+                pixels[i][j].G = 255;
+                pixels[i][j].B = 0;
+            }
+        }
+    }
+
+    void makeMagenta()
+    {
+        if (pixels == nullptr)
+        {
+            cerr << "Error: Image data is not loaded." << endl;
+            return;
+        }
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                pixels[i][j].R = 255;
+                pixels[i][j].G = 0;
+                pixels[i][j].B = 255;
+            }
+        }
+    }
+
+    void makeCyan()
+    {
+        if (pixels == nullptr)
+        {
+            cerr << "Error: Image data is not loaded." << endl;
+            return;
+        }
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                pixels[i][j].R = 0;
+                pixels[i][j].G = 255;
+                pixels[i][j].B = 255;
+            }
+        }
+    }
+
+    void makeGreen()
+    {
+        if (pixels == nullptr)
+        {
+            cerr << "Error: Image data is not loaded." << endl;
+            return;
+        }
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                pixels[i][j].R = 0;
+                pixels[i][j].G = 255;
+                pixels[i][j].B = 0;
+            }
+        }
+    }
+
+    void makeBlue()
+    {
+        if (pixels == nullptr)
+        {
+            cerr << "Error: Image data is not loaded." << endl;
+            return;
+        }
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                pixels[i][j].R = 0;
+                pixels[i][j].G = 0;
+                pixels[i][j].B = 255;
+            }
+        }
+    }
+
+    void turnImage()
+    {
+        if (pixels == nullptr)
+        {
+            cerr << "Error: Image data is not loaded." << endl;
+            return;
+        }
+
+        Pixel **newPixels = new Pixel *[width];
+        for (int i = 0; i < width; i++)
+            newPixels[i] = new Pixel[height];
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                newPixels[width - 1 - j][i] = pixels[i][j];
+            }
+        }
+
+        for (int i = 0; i < height; i++)
+            delete[] pixels[i];
+        delete[] pixels;
+
+        pixels = newPixels;
+        swap(width, height);
+    }
+
+    ~Image()
+    {
+        if (pixels != nullptr)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                delete[] pixels[i];
+            }
+            delete[] pixels;
+            pixels = nullptr;
+        }
+    }
+};
---
+++ code/A1/pixel.h
@@ -0,0 +1,7 @@
+#pragma once
+
+struct Pixel {      
+    unsigned char R;   
+    unsigned char G;   
+    unsigned char B;  
+};
