1e687140f0dc63476623f73f93a645edf9a88348
Talha-AppDev <talha.app.dev42@gmail.com>
2025-10-07 03:47:41 +0500

    changed lamp color

--- code/A1/image.h
+++ code/A1/image.h
@@ -5,17 +5,29 @@
 #include <sstream>
 #include <iostream>
 
-using namespace std;
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::getline;
+using std::ifstream;
+using std::ios;
+using std::max;
+using std::ofstream;
+using std::stoi;
+using std::string;
+using std::swap;
+using std::to_string;
 
 class Image
 {
-public:
     int width;
     int height;
     int maxColorValue;
     Pixel **pixels;
     string format;
 
+public:
     Image() : width(0), height(0), maxColorValue(255), pixels(nullptr), format("\0") {}
 
     Image(const Image &other)
@@ -223,6 +235,27 @@ public:
         return true;
     }
 
+    bool makeNegative()
+    {
+        if (pixels == nullptr)
+        {
+            cerr << "Error: Image data is not loaded." << endl;
+            return false;
+        }
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                pixels[i][j].R = 255 - pixels[i][j].R;
+                pixels[i][j].G = 255 - pixels[i][j].G;
+                pixels[i][j].B = 255 - pixels[i][j].B;
+            }
+        }
+
+        return true;
+    }
+
     bool makeMorph(const Image &img1, const Image &img2)
     {
         int i = 0;
@@ -258,6 +291,69 @@ public:
         cout << "Width: " << width << endl;
         cout << "Height: " << height << endl;
         cout << "Max Color Value: " << maxColorValue << endl;
+    }
+
+    bool changeLampColor(const Image &OnImage, const Image &OffImage, float r, float g, float b)
+    {
+        if (OnImage.pixels == nullptr || OffImage.pixels == nullptr)
+        {
+            cerr << "Error: Data of Images not loaded." << endl;
+            return false;
+        }
+
+        if (OnImage.format != OffImage.format)
+        {
+            cerr << "Error: Images formats do not match." << endl;
+            return false;
+        }
+
+        if (OnImage.width != OffImage.width || OnImage.height != OffImage.height)
+        {
+            cerr << "Error: Images dimensions do not match." << endl;
+            return false;
+        }
+
+        format = OnImage.format;
+        width = OnImage.width;
+        height = OnImage.height;
+        maxColorValue = OnImage.maxColorValue;
+
+        // allocate memory
+        Pixel **newPixels = new Pixel *[height];
+        for (int i = 0; i < height; i++)
+            newPixels[i] = new Pixel[width];
+
+        // process pixels
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                int lampR = max(0, OnImage.pixels[i][j].R - OffImage.pixels[i][j].R);
+                int lampG = max(0, OnImage.pixels[i][j].G - OffImage.pixels[i][j].G);
+                int lampB = max(0, OnImage.pixels[i][j].B - OffImage.pixels[i][j].B);
+
+                int red = OffImage.pixels[i][j].R + (lampR * r);
+                int green = OffImage.pixels[i][j].G + (lampG * g);
+                int blue = OffImage.pixels[i][j].B + (lampB * b);
+
+                newPixels[i][j].R = (red > 255) ? 255 : red;
+                newPixels[i][j].G = (green > 255) ? 255 : green;
+                newPixels[i][j].B = (blue > 255) ? 255 : blue;
+            }
+        }
+
+        // free old pixels if needed
+        if (pixels != nullptr)
+        {
+            for (int i = 0; i < height; i++)
+                delete[] pixels[i];
+            delete[] pixels;
+        }
+
+        // assign new image
+        pixels = newPixels;
+
+        return true;
     }
 
     bool writeFile(const string &filename)
--- code/A1/main.cpp
+++ code/A1/main.cpp
@@ -1,170 +1,144 @@
 #include <iostream>
-#include "image.h" // include your Image class
+#include "image.h"
 
-using namespace std;
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
 
 const string IMAGE_SAVE_PATH = "/home/talha/Desktop/Computer Graphics/Output/";
 const string IMAGE_LOAD_PATH = "/home/talha/Desktop/Computer Graphics/Input/";
 
-void showMenu()
-{
-    cout << "\n========== IMAGE MENU ==========\n";
-    cout << "1. Load Image\n";
-    cout << "2. Show Image Info\n";
-    cout << "3. Make Black\n";
-    cout << "4. Make White\n";
-    cout << "5. Make Red\n";
-    cout << "6. Make Green\n";
-    cout << "7. Make Blue\n";
-    cout << "8. Make Yellow\n";
-    cout << "9. Make Magenta\n";
-    cout << "10. Make Cyan\n";
-    cout << "11. Make Grayscale\n";
-    cout << "12. Turn Image\n";
-    cout << "13. Save Image\n";
-    cout << "14. Morph Images (Don't use option 1)\n";
-    cout << "0. Exit\n";
-    cout << "================================\n";
-    cout << "Enter choice: ";
+void clearScreen() {
+    system("clear"); 
 }
 
-int main()
-{
+void showMenu() {
+    cout << "\n========== IMAGE PROCESSING MENU ==========\n";
+    cout << " 1.  Load an Image\n";
+    cout << " 2.  Show Image Info\n";
+    cout << " 3.  Turn Image Black\n";
+    cout << " 4.  Turn Image White\n";
+    cout << " 5.  Turn Image Red\n";
+    cout << " 6.  Turn Image Green\n";
+    cout << " 7.  Turn Image Blue\n";
+    cout << " 8.  Turn Image Yellow\n";
+    cout << " 9.  Turn Image Magenta\n";
+    cout << "10.  Turn Image Cyan\n";
+    cout << "11.  Convert to Grayscale & Save\n";
+    cout << "12.  Rotate Image \n";
+    cout << "13.  Save Current Image\n";
+    cout << "14.  Load, Process & Save Morph Images\n";
+    cout << "15.  Create Negative Image\n";
+    cout << "16.  Change Lamp Color (Light ON/OFF)\n";
+    cout << " 0.  Exit\n";
+    cout << "===========================================\n";
+    cout << "Enter your choice: ";
+}
 
+int main() {
     Image img, I1, I2;
     string filename, img1, img2;
     int choice;
 
-    do
-    {
+    do {
         showMenu();
         cin >> choice;
+        clearScreen();
 
-        switch (choice)
-        {
-        case 1:
-            cout << "Enter filename to load: ";
-            cin >> filename;
-            if (img.readFile(IMAGE_LOAD_PATH + filename))
-            {
-                system("clear");
-                cout << "Image loaded successfully!\n";
+        switch (choice) {
+            case 1: {
+                cout << "Enter filename to load: ";
+                cin >> filename;
+                if (img.readFile(IMAGE_LOAD_PATH + filename))
+                    cout << "Image loaded successfully!\n";
+                else
+                    cout << "Failed to load image.\n";
+                break;
             }
-            else
-                cout << "Failed to load image.\n";
-            break;
+            case 2: img.showInfo(); break;
+            case 3: img.makeBlack();   cout << "Image turned Black!\n"; break;
+            case 4: img.makeWhite();   cout << "Image turned White!\n"; break;
+            case 5: img.makeRed();     cout << "Image turned Red!\n"; break;
+            case 6: img.makeGreen();   cout << "Image turned Green!\n"; break;
+            case 7: img.makeBlue();    cout << "Image turned Blue!\n"; break;
+            case 8: img.makeYellow();  cout << "Image turned Yellow!\n"; break;
+            case 9: img.makeMagenta(); cout << "Image turned Magenta!\n"; break;
+            case 10: img.makeCyan();   cout << "Image turned Cyan!\n"; break;
 
-        case 2:
-            system("clear");
-            img.showInfo();
-            break;
-
-        case 3:
-            system("clear");
-            img.makeBlack();
-            cout << "Image turned Black!\n";
-            break;
-
-        case 4:
-            system("clear");
-            img.makeWhite();
-            cout << "Image turned White!\n";
-            break;
-
-        case 5:
-            system("clear");
-            img.makeRed();
-            cout << "Image turned Red!\n";
-            break;
-
-        case 6:
-            system("clear");
-            img.makeGreen();
-            cout << "Image turned Green!\n";
-            break;
-
-        case 7:
-            system("clear");
-            img.makeBlue();
-            cout << "Image turned Blue!\n";
-            break;
-
-        case 8:
-            system("clear");
-            img.makeYellow();
-            cout << "Image turned Yellow!\n";
-            break;
-
-        case 9:
-            system("clear");
-            img.makeMagenta();
-            cout << "Image turned Magenta!\n";
-            break;
-
-        case 10:
-            system("clear");
-            img.makeCyan();
-            cout << "Image turned Cyan!\n";
-            break;
-
-        case 11:
-
-            cout << "Enter filename to save grayscale img: ";
-            cin >> filename;
-            if (img.makeGrayscale(IMAGE_SAVE_PATH + filename))
-            {
-                system("clear");
-                cout << "Grayscale Image saved successfully!\n";
+            case 11: {
+                cout << "Enter filename to save grayscale image: ";
+                cin >> filename;
+                if (img.makeGrayscale(IMAGE_SAVE_PATH + filename))
+                    cout << "Grayscale image saved successfully!\n";
+                else
+                    cout << "Failed to save grayscale image.\n";
+                break;
             }
-            else
-                cout << "Failed to save image.\n";
-            break;
 
-        case 12:
-            system("clear");
-            img.turnImage();
-            cout << "Image turned 90 degrees counter-clockwise!\n";
-            break;
-
-        case 13:
-
-            cout << "Enter filename to save: ";
-            cin >> filename;
-            if (img.writeFile(IMAGE_SAVE_PATH + filename))
-            {
-                system("clear");
-                cout << "Image saved successfully!\n";
+            case 12: {
+                img.turnImage();
+                cout << "Image rotated 90° counter-clockwise!\n";
+                break;
             }
-            else
-                cout << "Failed to save image.\n";
-            break;
 
-        case 14:
+            case 13: {
+                cout << "Enter filename to save: ";
+                cin >> filename;
+                if (img.writeFile(IMAGE_SAVE_PATH + filename))
+                    cout << "Image saved successfully!\n";
+                else
+                    cout << "Failed to save image.\n";
+                break;
+            }
 
-            cout << "Enter first image filename to load: ";
-            cin >> img1;
-            I1.readFile(IMAGE_LOAD_PATH + img1);
-            I1.showInfo();
+            case 14: {
+                cout << "Enter first image filename: ";
+                cin >> img1;
+                I1.readFile(IMAGE_LOAD_PATH + img1);
 
-            cout << "Enter second image filename to load: ";
-            cin >> img2;
-            I2.readFile(IMAGE_LOAD_PATH + img2);
-            I2.showInfo();
+                cout << "Enter second image filename: ";
+                cin >> img2;
+                I2.readFile(IMAGE_LOAD_PATH + img2);
 
-            
-            if (img.makeMorph(I1, I2))
-                cout << "Morph images created successfully!\n";
-            else
-                cout << "Failed to create morph images.\n";
-            break;
+                if (img.makeMorph(I1, I2))
+                    cout << "Morph images created successfully!\n";
+                else
+                    cout << "Failed to create morph images.\n";
+                break;
+            }
 
-        case 0:
-            system("clear");
-            cout << "Exiting program...\n";
-            break;
+            case 15: {
+                if (img.makeNegative())
+                    cout << "Negative image created successfully!\n";
+                else
+                    cout << "Failed to create negative image.\n";
+                break;
+            }
 
-        default:
-            cout << "Invalid choice. Try again.\n";
+            case 16: {
+                cout << "Enter Light ON image filename: ";
+                cin >> img1;
+                I1.readFile(IMAGE_LOAD_PATH + img1);
+
+                cout << "Enter Light OFF image filename: ";
+                cin >> img2;
+                I2.readFile(IMAGE_LOAD_PATH + img2);
+
+                float r, g, b;
+                cout << "Enter scaling factors for (R G B), e.g., 1.0 0.5 0.2: ";
+                cin >> r >> g >> b;
+
+                if (img.changeLampColor(I1, I2, r, g, b))
+                    cout << "Lamp color changed successfully!\n";
+                else
+                    cout << "Failed to change lamp color.\n";
+                break;
+            }
+
+            case 0: cout << "Exiting program...\n"; break;
+            default: cout << "Invalid choice. Try again.\n";
         }
 
     } while (choice != 0);
